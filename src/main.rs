322d768b1b4f5f5182d96cab397512fe7dd40899
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-05-11 10:23:41 +0700

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use notify_rust::Notification;
+use std::fs;
+use dirs::home_dir;
+use chrono::{Local, NaiveDate, Datelike, offset::TimeZone};
+
+fn main() {
+    let path = home_dir().unwrap();
+    let today = Local::today();
+    let calendar = fs::read_to_string(path.into_os_string().into_string().unwrap() + "/calendar.txt")
+        .expect("Something went wrong reading the file");
+    let calendar_line = calendar.lines();
+    for (_i, line) in calendar_line.enumerate() {
+        let mut iter = line.split(":");
+        let name = iter.next().unwrap();
+        let date_str = iter.next().unwrap();
+
+        let date_year = format!("{} {}", date_str, today.year());
+
+        let date = NaiveDate::parse_from_str(&date_year, "%d %B %Y").unwrap();
+
+        if Local.from_local_date(&date).unwrap() == today {
+            Notification::new()
+            .summary("Today's event")
+            .body(&"Today is {}'s birthday! Tell them HBD.".replace("{}", name))
+            .show();
+        }
+
+    }
+    
+}
